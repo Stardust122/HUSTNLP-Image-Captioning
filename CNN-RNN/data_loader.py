2ed7953d2f3b40d86782c66ea622c4846b682e27
Sitian Gu <87805922+Stardust122@users.noreply.github.com>
2024-08-20 11:07:01 +0800

    Add files via upload

---
+++ CNN-RNN/data_loader.py
@@ -0,0 +1,133 @@
+import json
+import os
+import nltk
+import numpy as np
+import torch
+import torch.utils.data as data
+from PIL import Image
+from pycocotools.coco import COCO
+from tqdm import tqdm
+from vocabulary import Vocabulary
+
+
+def get_loader(transform,
+               mode='train',
+               batch_size=1,
+               vocab_threshold=None,
+               vocab_file='./vocab_simple.pkl',
+               start_word="<start>",
+               end_word="<end>",
+               unk_word="<unk>",
+               vocab_from_file=True,
+               num_workers=0):
+
+    # 根据模式选择图像和注释文件路径
+    if mode == 'train':
+        img_folder = 'E:/nlp/final'
+        annotations_file = r'E:\nlp\final\CNN-RNN\coco_annotations_train_simple.json'
+    if mode == 'test':
+        img_folder = 'E:/nlp/final'
+        annotations_file = r'E:\nlp\final\CNN-RNN\coco_annotations_val_simple.json'
+
+    # 初始化COCO数据集
+    dataset = CoCoDataset(transform=transform,
+                          mode=mode,
+                          batch_size=batch_size,
+                          vocab_threshold=vocab_threshold,
+                          vocab_file=vocab_file,
+                          start_word=start_word,
+                          end_word=end_word,
+                          unk_word=unk_word,
+                          annotations_file=annotations_file,
+                          vocab_from_file=vocab_from_file,
+                          img_folder=img_folder)
+
+    if mode == 'train':
+        # 采样一个长度相同的caption，并使用该长度采样索引
+        indices = dataset.get_train_indices()
+        # 为采样索引创建并分配批次采样器
+        initial_sampler = data.sampler.SubsetRandomSampler(indices=indices)
+        # 数据加载器
+        data_loader = data.DataLoader(dataset=dataset,
+                                      num_workers=num_workers,
+                                      batch_sampler=data.sampler.BatchSampler(sampler=initial_sampler,
+                                                                              batch_size=dataset.batch_size,
+                                                                              drop_last=False))
+    else:
+        # 测试模式的数据加载器
+        data_loader = data.DataLoader(dataset=dataset,
+                                      batch_size=dataset.batch_size,
+                                      shuffle=True,
+                                      num_workers=num_workers)
+
+    return data_loader
+
+
+class CoCoDataset(data.Dataset):
+
+    def __init__(self, transform, mode, batch_size, vocab_threshold, vocab_file, start_word,
+                 end_word, unk_word, annotations_file, vocab_from_file, img_folder):
+        self.transform = transform
+        self.mode = mode
+        self.batch_size = batch_size
+        self.vocab = Vocabulary(vocab_threshold, vocab_file, start_word,
+                                end_word, unk_word, annotations_file, vocab_from_file)
+        self.img_folder = img_folder
+        if self.mode == 'train':
+            self.coco = COCO(annotations_file)
+            self.ids = list(self.coco.anns.keys())
+            print('获取caption长度...')
+            all_tokens = [nltk.tokenize.word_tokenize(str(self.coco.anns[self.ids[index]]['caption']).lower()) for index
+                          in tqdm(np.arange(len(self.ids)))]
+            self.caption_lengths = [len(token) for token in all_tokens]
+        else:
+            test_info = json.loads(open(annotations_file).read())
+            self.paths = [item['file_name'] for item in test_info['images']]
+
+    def __getitem__(self, index):
+        # 训练模式下获取图像和caption
+        if self.mode == 'train':
+            ann_id = self.ids[index]
+            caption = self.coco.anns[ann_id]['caption']
+            img_id = self.coco.anns[ann_id]['image_id']
+            path = self.coco.loadImgs(img_id)[0]['file_name']
+
+            # 读取并转换图像
+            image = Image.open(os.path.join(self.img_folder, path)).convert('RGB')
+            image = self.transform(image)
+
+            # 将caption转换为词id的张量
+            tokens = nltk.tokenize.word_tokenize(str(caption).lower())
+            caption = []
+            caption.append(self.vocab(self.vocab.start_word))
+            caption.extend([self.vocab(token) for token in tokens])
+            caption.append(self.vocab(self.vocab.end_word))
+            caption = torch.Tensor(caption).long()
+
+            # 返回处理后的图像和caption张量
+            return image, caption
+
+        # 测试模式下获取图像
+        else:
+            path = self.paths[index]
+
+            # 读取并转换图像
+            PIL_image = Image.open(os.path.join(self.img_folder, path)).convert('RGB')
+            orig_image = np.array(PIL_image)
+            image = self.transform(PIL_image)
+
+            # 返回原始图像和处理后的图像张量
+            return orig_image, image
+
+    def get_train_indices(self):
+        # 随机选择一个caption长度，并获取对应长度的索引
+        sel_length = np.random.choice(self.caption_lengths)
+        all_indices = np.where([self.caption_lengths[i] == sel_length for i in np.arange(len(self.caption_lengths))])[0]
+        indices = list(np.random.choice(all_indices, size=self.batch_size))
+        return indices
+
+    def __len__(self):
+        if self.mode == 'train':
+            return len(self.ids)
+        else:
+            return len(self.paths)
